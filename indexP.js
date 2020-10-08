45f684f7944cba6f45cb49e9913444362af177e2
schraderbachar <44781569+schraderbachar@users.noreply.github.com>
2020-10-08 14:22:26 -0500

    Add files via upload

---
+++ indexP.js
@@ -0,0 +1,27 @@
+//this part is acutally what changes the elements
+
+
+//access the document and search for each of the three ids and use the onlick attribute to call the function- dont need the () on the function call.
+
+
+//functions after the elements
+function turnRed() {
+    //call the clear function
+
+//change the color of the element to Red
+}
+function turnYellow() {
+//call the clear function
+
+//change the color of the element to Yellow
+}
+function turnGreen() {
+//call the clear function
+
+//change the color of the element to Green
+}
+function clear(){
+    // you can use the single or double in js- also the semi colon doesnt matter but i like to put it there just because it offers better readability
+
+ //access the elements by the ID (each of them 3 lines) and change the background color to the same as the style (#333)
+}
